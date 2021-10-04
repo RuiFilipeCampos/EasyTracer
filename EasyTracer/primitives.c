9184caf1a193cda1cc518a38200155db7a1c63f6
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-04 12:53:14 +0100

    new files

---
+++ EasyTracer/primitives.c
@@ -0,0 +1,51 @@
+
+#include "primitives.h"
+#include "types.h"
+
+
+
+
+
+
+/*********
+ * 
+ * 
+ *    SPHERES
+ * 
+ * 
+ * ******/
+
+
+
+double intersect_sphere(void *this, double3 *origin, double3 *direction){
+    Sphere *sphere = (Sphere *) this;
+    return 1; 
+};
+
+Sphere new_Sphere(double x, double y, double z, double radius){
+    /*
+    PARAMETERS:
+        COORDINATES OF THE CENTER OF THE SPHERE (cm):
+            double x
+            double y
+            double z
+
+        double radius: radius of sphere in cm
+    */
+
+    Sphere sphere;
+
+    // geometric parameters
+    sphere.center.x = x;
+    sphere.center.y = y;
+    sphere.center.z = z;
+    sphere.radius = radius;
+
+    // base stuff
+
+    sphere.base.intersect = &intersect_sphere;
+
+
+
+};
+
---
+++ EasyTracer/types.h
@@ -0,0 +1,11 @@
+#ifndef _TYPESH_
+#define _TYPESH_
+
+// a 3 dimensional vector
+typedef struct double3{
+	double x;
+	double y;
+	double z;
+} double3;
+
+#endif
