cf4ab99f356eb70605485ef645b4bf632c6fc0a2
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-04 14:53:41 +0100

    was missing some files

---
+++ EasyTracer/types.c
@@ -0,0 +1,18 @@
+
+#include "types.h"
+
+double dot_product(double3 A, double3 B){ 
+    return A.x*B.x + A.y*B.y + A.z*B.z;
+    };
+
+
+double3 subtract_vectors(double3 A, double3 B){ 
+    double3 result;
+
+
+    result.x = A.x - B.x;
+    result.y = A.y - B.y;
+    result.z = A.z - B.z;
+    return result; 
+    
+    };
